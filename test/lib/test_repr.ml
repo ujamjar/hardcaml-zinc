b60c2ad14497d35957ed7083514e309a746ed7a1
Andy Ray <andy.ray@ujamjar.com>
2019-12-26 00:07:26 +0000

    more Repr tests - roundtrip via Obj.t and Repr.t

--- test/lib/test_repr.ml
+++ test/lib/test_repr.ml
@@ -1,5 +1,7 @@
+open Base
 open Hardcaml_zinc
 open Expect_test_helpers_kernel
+module Obj = Caml.Obj
 
 let%expect_test "[Repr.of_obj] Int" =
   let zero = Repr.of_obj (Obj.repr 0) in
@@ -22,26 +24,27 @@ let%expect_test "[Repr.of_obj] String" =
     {| (string (Flat (header 2300) (data (0x6f77206f6c6c6568 0x400000000646c72)))) |}]
 ;;
 
+type x =
+  { a : int
+  ; b : x option
+  }
+[@@deriving sexp_of]
+
 let%expect_test "[Repr.of_obj] Record" =
-  let module X = struct
-    type t =
-      { a : int
-      ; b : t option
-      }
-  end
-  in
-  let record = { X.a = 20; b = None } |> Obj.repr |> Repr.of_obj in
+  let record = { a = 20; b = None } |> Obj.repr |> Repr.of_obj in
   print_s [%message (record : Repr.t)];
-  [%expect {|
+  [%expect
+    {|
     (record (
       Block
       (header 2048)
       (data (
         (Int 41)
         (Int 1))))) |}];
-  let record = { X.a = 64; b = Some { a = 12; b = None } } |> Obj.repr |> Repr.of_obj in
+  let record = { a = 64; b = Some { a = 12; b = None } } |> Obj.repr |> Repr.of_obj in
   print_s [%message (record : Repr.t)];
-  [%expect {|
+  [%expect
+    {|
     (record (
       Block
       (header 2048)
@@ -55,4 +58,34 @@ let%expect_test "[Repr.of_obj] Record" =
             (data (
               (Int 25)
               (Int 1))))))))))) |}]
+;;
+
+let roundtrip x = x |> Obj.repr |> Repr.of_obj |> Repr.to_obj |> Obj.magic
+
+let%expect_test "Rountrip via [Repr.t], int" =
+  let zero = roundtrip 0 in
+  let one = roundtrip 1 in
+  let two = roundtrip 2 in
+  print_s [%message (zero : int) (one : int) (two : int)];
+  [%expect {|
+    ((zero 0)
+     (one  1)
+     (two  2)) |}]
+;;
+
+let%expect_test "Roundtrip via [Repr.t], string" =
+  let string = roundtrip "hello world" in
+  print_s [%message (string : string)];
+  [%expect {| (string "hello world") |}]
+;;
+
+let%expect_test "Roundtrip via [Repr.t], record " =
+  let record = roundtrip { a = 20; b = None } in
+  print_s [%message (record : x)];
+  [%expect
+    {|
+    (record ((a 20) (b ()))) |}];
+  let record = roundtrip { a = 64; b = Some { a = 12; b = None } } in
+  print_s [%message (record : x)];
+  [%expect {| (record ((a 64) (b (((a 12) (b ())))))) |}]
 ;;
